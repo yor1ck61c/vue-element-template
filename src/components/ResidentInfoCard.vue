a91aff9016507874700c7fdeaa1dc0a5d03060f3
yorick_61c <1240371570@qq.com>
2021-01-16 22:12:02 +0800

    新增了组件功能，编写了部分个人信息。

---
+++ src/components/ResidentInfoCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <p>Github: <el-link :href="github">{{ github }}</el-link></p>
+</template>
+
+<script>
+export default {
+  name: 'ResidentInfoCard',
+  data() {
+    return {
+      'github': 'https://github.com/yor1ck61c'
+    }
+  }
+}
+</script>
