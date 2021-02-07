ca2717d035fb750dbd758693c977107f630e595b
ScottSong0714 <76896600+ScottSong0714@users.noreply.github.com>
2021-02-07 23:00:01 +0800

    更新字体
    更新字体

--- src/views/HealthInterfere/HealthReport.vue
+++ src/views/HealthInterfere/HealthReport.vue
@@ -4,57 +4,47 @@
       <el-row>
         <el-col>
           <el-tabs v-model="activeCard">
-            <el-tab-pane label="待处理预警" name="NeedHandleAlert">
+            <el-tab-pane label="待审报告" name="NeedCheckReport">
               <div>
-                <el-input v-model="NeedHandleAlertInfo.Name" placeholder="请输入姓名" style="width: 150px;" />
-                <el-date-picker v-model="NeedHandleAlertInfo.StartTime" type="date" placeholder="开始时间" style="width: 150px; margin-left: 20px;" />
-                <el-date-picker v-model="NeedHandleAlertInfo.EndTime" type="date" placeholder="结束时间" style="width: 150px; margin-left: 20px;" />
-                <el-input v-model="NeedHandleAlertInfo.BuildHospital" placeholder="请输入建档机构" style="width: 200px; margin-left: 20px;" />
-                <el-button type="warning" style="margin-left: 20px;">搜 索</el-button>
+                <el-input v-model="NeedCheckReportSearch.Name" placeholder="请输入姓名" style="width: 150px;" />
+                <el-date-picker v-model="NeedCheckReportSearch.Month" placeholder="请选择月份" style="width: 150px; margin-left: 20px;" />
+                <el-button type="warning" style="margin-left: 20px;">搜索</el-button>
+                <el-button type="primary" style="margin-left: 20px;">全部</el-button>
+                <el-button type="primary" style="margin-left: 20px;">上月</el-button>
+                <el-button type="primary" style="margin-left: 20px;">批量审核报告</el-button>
               </div>
               <div>
-                <el-table :data="NeedHandleAlertTable" border style="margin-top: 20px;">
+                <el-table :data="NeedCheckHealthReportTable" border style="margin-top: 20px; font-size: 12px">
                   <el-table-column label="操作" align="center">
-                    <el-button size="mini" type="primary">处 理</el-button>
+                    <el-button size="mini" type="primary" @click="CheckReport()">审核报告</el-button>
                   </el-table-column>
                   <el-table-column prop="Name" label="姓名" align="center" />
-                  <el-table-column prop="ID" label="身份证号" align="center" />
                   <el-table-column prop="Gender" label="性别" align="center" />
                   <el-table-column prop="Age" label="年龄" align="center" />
                   <el-table-column prop="PhoneNum" label="联系电话" align="center" />
+                  <el-table-column prop="ReportCycle" label="报告周期" align="center" />
                   <el-table-column prop="BuildHospital" label="建档机构" align="center" />
                   <el-table-column prop="Address" label="户籍地址" align="center" />
-                  <el-table-column prop="ExamTarget" label="检测指标" align="center" />
-                  <el-table-column prop="AlertType" label="预警类型" align="center" />
-                  <el-table-column prop="ExamValue" label="检测值" align="center" />
-                  <el-table-column prop="AnalyseRes" label="分析结果" align="center" />
-                  <el-table-column prop="AlertTime" label="预警时间" align="center" />
                 </el-table>
               </div>
             </el-tab-pane>
-            <el-tab-pane label="已处理预警" name="AlreadyHandleAlert">
+            <el-tab-pane label="已审报告" name="AlreadyCheckReport">
               <div>
-                <el-input v-model="AlreadyHandleAlertInfo.Name" placeholder="请输入姓名" style="width: 150px;" />
-                <el-date-picker v-model="AlreadyHandleAlertInfo.StartTime" type="date" placeholder="开始时间" style="width: 150px; margin-left: 20px;" />
-                <el-date-picker v-model="AlreadyHandleAlertInfo.EndTime" type="date" placeholder="结束时间" style="width: 150px; margin-left: 20px;" />
-                <el-input v-model="AlreadyHandleAlertInfo.BuildHospital" placeholder="请输入建档机构" style="width: 200px; margin-left: 20px;" />
-                <el-button type="warning" style="margin-left: 20px;">搜 索</el-button>
-                <el-button type="primary" style="margin-left: 20px;">导出Excel表格</el-button>
+                <el-input v-model="AlreadyCheckReportSearch.Name" placeholder="请输入姓名" style="width: 150px;" />
+                <el-date-picker v-model="AlreadyCheckReportSearch.Month" placeholder="请选择月份" style="width: 150px; margin-left: 20px;" />
+                <el-button type="warning" style="margin-left: 20px;">搜索</el-button>
+                <el-button type="primary" style="margin-left: 20px;">全部</el-button>
+                <el-button type="primary" style="margin-left: 20px;">上月</el-button>
               </div>
               <div>
-                <el-table :data="AlreadyHandleAlertTable" border style="margin-top: 20px;">
+                <el-table :data="AlreadyCheckHealthReportTable" border style="margin-top: 20px; font-size: 12px">
                   <el-table-column prop="Name" label="姓名" align="center" />
-                  <el-table-column prop="ID" label="身份证号" align="center" />
                   <el-table-column prop="Gender" label="性别" align="center" />
                   <el-table-column prop="Age" label="年龄" align="center" />
                   <el-table-column prop="PhoneNum" label="联系电话" align="center" />
+                  <el-table-column prop="ReportCycle" label="报告周期" align="center" />
                   <el-table-column prop="BuildHospital" label="建档机构" align="center" />
                   <el-table-column prop="Address" label="户籍地址" align="center" />
-                  <el-table-column prop="ExamTarget" label="检测指标" align="center" />
-                  <el-table-column prop="AlertType" label="预警类型" align="center" />
-                  <el-table-column prop="ExamValue" label="检测值" align="center" />
-                  <el-table-column prop="AnalyseRes" label="分析结果" align="center" />
-                  <el-table-column prop="AlertTime" label="预警时间" align="center" />
                 </el-table>
               </div>
             </el-tab-pane>
@@ -66,15 +56,35 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HealthReport',
   data() {
     return {
-      activeCard: 'NeedHandleAlert',
-      NeedHandleAlertInfo: {},
-      NeedHandleAlertTable: [],
-      AlreadyHandleAlertInfo: {},
-      AlreadyHandleAlertTable: []
+      activeCard: 'NeedCheckReport',
+      NeedCheckReportSearch: {},
+      NeedCheckHealthReportTable: [],
+      AlreadyCheckReportSearch: {},
+      AlreadyCheckHealthReportTable: []
+    }
+  },
+  created() {
+    this.getNeedCheckHealthReportTableInfo()
+  },
+  methods: {
+    getNeedCheckHealthReportTableInfo: function() {
+      axios.post('https://www.fastmock.site/mock/16fddfe65af12b42183595cffa0358a8/getDeviceData/getUnCheck').then((res) => {
+        this.NeedCheckHealthReportTable = res.data.NeedCheckHealthReportTable
+      })
+    },
+    CheckReport: function() {
+      this.$router.push({
+        path: '/CheckReport',
+        name: 'CheckReport',
+        params: {
+          'Name': this.NeedCheckHealthReportTable.Name
+        }
+      })
     }
   }
 }
