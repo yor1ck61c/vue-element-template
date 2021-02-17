488a6328e4950747057f12f793af1f28bff880fa
yorick_61c <1240371570@qq.com>
2021-02-17 19:58:33 +0800

    完成前端权限验证功能，可根据用户的角色动态加载路由

--- src/store/index.js
+++ src/store/index.js
@@ -1,20 +1,30 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import getters from './getters'
-import app from './modules/app'
-import settings from './modules/settings'
-import user from './modules/user'
-import tagsView from './modules/tagsView'
 
 Vue.use(Vuex)
 
+// https://webpack.js.org/guides/dependency-management/#requirecontext
+// 简化文件导入，批量引入
+const modulesFiles = require.context('./modules', true, /\.js$/)
+
+// you do not need `import app from './modules/app'`
+// it will auto require all vuex module from modules file
+const modules = modulesFiles.keys().reduce((modules, modulePath) => {
+  /*
+  * keys() 方法用于从数组创建一个包含数组键的可迭代对象。
+  * 如果对象是数组返回 true，否则返回 false。
+  *
+  */
+  // set './app.js' => 'app'
+  const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
+  const value = modulesFiles(modulePath)
+  modules[moduleName] = value.default
+  return modules
+}, {})
+
 const store = new Vuex.Store({
-  modules: {
-    app,
-    settings,
-    tagsView,
-    user
-  },
+  modules,
   getters
 })
 
